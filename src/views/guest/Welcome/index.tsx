e9f0ec2055ffeb84ccb1584b04049acfa5f41ca6
tpellegrin <tgpellegrin@gmail.com>
2026-01-21 10:14:45 -0300

    chore: update Welcome page

--- src/views/guest/Welcome/index.tsx
+++ src/views/guest/Welcome/index.tsx
@@ -1,31 +1,51 @@
 import { useNavigate } from 'react-router';
 
 import { Flex } from 'components/Flex';
+import { Text } from 'components/Text';
 import { FromBelowReveal } from 'components/Animations/FromBelowReveal';
 import { Button } from 'components/Button';
 import { paths } from 'globals/paths';
+import { MainLayout } from 'containers/Layouts/MainLayout';
 
 export function Welcome() {
   const navigate = useNavigate();
 
+  // TODO: move texts to i18n
   return (
-    <Flex
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      height="100vh"
-      width="100%"
-    >
-      <FromBelowReveal delayMs={400}>
-        <h1>Welcome (guest)</h1>
-        <p>This is the welcome page for guests</p>
-        <Button
-          variant="tertiary"
-          size="small"
-          label="Calculator"
-          onClick={() => navigate(paths.flow.calculator.intro)}
-        />
-      </FromBelowReveal>
-    </Flex>
+    <MainLayout contentInnerClassName="page-padding">
+      <Flex
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+        width="100%"
+      >
+        <FromBelowReveal delayMs={400}>
+          <Flex gap="lg">
+            <Text variant="accentMd">
+              Este app foi criado para cuidar daqueles que fazem a missão
+              acontecer.
+            </Text>
+            <Text variant="accentMd">
+              Por meio dessas perguntas, queremos entender como você está —
+              emocionalmente, fisicamente e espiritualmente.
+            </Text>
+            <Text variant="accentMd">
+              Responda com sinceridade. Não há respostas certas ou erradas.
+            </Text>
+            <Text variant="accentMd">
+              O mais importante é que suas respostas representem a sua realidade
+              hoje.
+            </Text>
+            <Button
+              variant="tertiary"
+              size="small"
+              label="Iniciar"
+              onClick={() => navigate(paths.flow.calculator.intro)}
+            />
+          </Flex>
+        </FromBelowReveal>
+      </Flex>
+    </MainLayout>
   );
 }
